6e0967a6702feb1e3f440ea010a268d06e953faf
Pei Xiong Liu <peixiong.liu@gmail.com>
2016-10-13 13:52:19 -0400

    Install and set up firebase

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { firebase } from 'firebase';
+
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBhSGzitMFdMNNEYuSbksrRV9CA-f-Liks',
+      authDomain: 'react-native-auth-tutorial.firebaseapp.com',
+      databaseURL: 'https://react-native-auth-tutorial.firebaseio.com',
+      storageBucket: 'react-native-auth-tutorial.appspot.com',
+      messagingSenderId: '587398671555'
+    });
+  }
+
   render() {
     return (
       <View>
