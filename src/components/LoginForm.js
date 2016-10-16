b0f39c0c1ade99939cdea08fa67766d80701248e
Pei Xiong Liu <peixiong.liu@gmail.com>
2016-10-16 16:15:55 -0400

    Get firebase auth working

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+import firebase from 'firebase';
 import { Button, CardSection, Card, Input } from './common';
 
 class LoginForm extends Component {
   state = {
     email: '',
-    password: ''
+    password: '',
+    error: '',
   };
 
+  onButtonPress() {
+    const { email, password } = this.state;
+    this.setState({ error: '' });
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password).catch(() => {
+          this.setState({ error: 'Authentication Failed.' });
+        });
+      });
+  }
+
   render() {
+    const { errorTextStyle } = styles;
     return (
       <Card>
         <CardSection>
@@ -28,14 +43,26 @@ class LoginForm extends Component {
             secureTextEntry
           />
         </CardSection>
+        <Text style={errorTextStyle}>
+          {this.state.error}
+        </Text>
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Log In
           </Button>
         </CardSection>
       </Card>
     );
   }
+
 }
+
+const styles = {
+  errorTextStyle: {
+    color: 'red',
+    fontSize: 20,
+    alignSelf: 'center'
+  }
+};
 
 export default LoginForm;
