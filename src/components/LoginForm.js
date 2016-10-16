9e3d697eb6d3f88d5590d4001033b3b811ddcfcc
Pei Xiong Liu <peixiong.liu@gmail.com>
2016-10-16 01:24:29 -0400

    Use Input component in LoginForm

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
-import { Button, CardSection, Card } from './common';
+import { TextInput } from 'react-native';
+import { Button, CardSection, Card, Input } from './common';
 
 class LoginForm extends Component {
+  state = {
+    text: ''
+  };
+
   render() {
     return (
       <Card>
-        <CardSection />
+        <CardSection>
+          <Input
+            value={this.state.text}
+            onChangeText={text => this.setState({ text })}
+          />
+        </CardSection>
+
         <CardSection />
         <CardSection>
           <Button>
