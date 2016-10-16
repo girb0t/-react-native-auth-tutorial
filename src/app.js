25391b92a8aee21c770820c4be24e26aa4465578
Pei Xiong Liu <peixiong.liu@gmail.com>
2016-10-16 16:54:43 -0400

    Create log out view

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
 
-import { Header } from './components/common';
+import { Header, Button } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: false }
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyBhSGzitMFdMNNEYuSbksrRV9CA-f-Liks',
@@ -14,13 +16,33 @@ class App extends Component {
       storageBucket: 'react-native-auth-tutorial.appspot.com',
       messagingSenderId: '587398671555'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    if (this.state.loggedIn) {
+      return (
+        <Button>
+          Log Out
+        </Button>
+      );
+    }
+
+    return <LoginForm />;
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
