14ea52ead2da87d325f5714e8ab0e6acbd329c14
Pei Xiong Liu <peixiong.liu@gmail.com>
2016-10-16 01:38:16 -0400

    Add styling to Input

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import { TextInput, View, Text } from 'react-native';
 
-const Input = ({ label, onChangeText }) => {
+const Input = ({ value, label, onChangeText }) => {
+  const { inputStyle, labelStyle, containerStyle } = styles;
   return (
-    <View>
-      <Text>{label}</Text>
+    <View style={containerStyle}>
+      <Text style={labelStyle}>{label}</Text>
       <TextInput
-        style={{ height: 20, width: 100 }}
+        value={value}
+        style={inputStyle}
         onChangeText={onChangeText}
       />
     </View>
   );
+};
+
+const styles = {
+  inputStyle: {
+    color: '#000',
+    paddingRight: 5,
+    paddingLeft: 5,
+    fontSize: 18,
+    lineHeight: 23,
+    flex: 2
+  },
+  labelStyle: {
+    fontSize: 18,
+    paddingLeft: 20,
+    flex: 1
+  },
+  containerStyle: {
+    height: 40,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center'
+  }
 };
 
 export { Input };
